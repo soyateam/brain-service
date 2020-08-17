ecd895062d5e8ba8520c9d0fddfb667681a0d76f
Shaked Manes <maneshaked@gmail.com>
2020-08-17 15:20:20 +0300

    feat: Add group and task manager for using group and task api decoupled from controller

---
+++ src/group/group.manager.ts
@@ -0,0 +1,26 @@
+// group.manager
+
+import config from '../config';
+import { HttpClient } from '../utils/http.client';
+
+export class GroupManager {
+
+  private static readonly groupsUrl = `${config.groupServiceUrl}/api/group`;
+
+  /**
+   * Get group by id.
+   * @param groupId - The id of the group.
+   */
+  public static async getGroupById(groupId: string) {
+    return (await HttpClient.get(`${GroupManager.groupsUrl}/${groupId}`));
+  }
+
+  /**
+   * Get multiple groups by array of group ids.
+   * @param groupsIds - Array of group ids.
+   */
+  public static async getManyGroups(groupsIds: string) {
+    // TODO: Update it with real API request
+    return [];
+  }
+}
---
+++ src/task/task.manager.ts
@@ -0,0 +1,26 @@
+// task.manager
+
+import { HttpClient } from '../utils/http.client';
+import config from '../config';
+
+export class TaskManager {
+
+  private static readonly tasksUrl = `${config.taskServiceUrl}/task`;
+
+  /**
+   * Gets all children tasks of a given task by its id.
+   * @param parentId - The id of the parent task.
+   */
+  public static async getTasksByParentId(parentId: string) {
+    return (await HttpClient.get(`${TaskManager.tasksUrl}/parent/${parentId}`)).tasks;
+  }
+
+  /**
+   * Gets a task by Id.
+   * @param taskId - The id of the task.
+   */
+  public static async getTaskById(taskId: string) {
+    return await HttpClient.get(`${TaskManager.tasksUrl}/${taskId}`);
+  }
+
+}
